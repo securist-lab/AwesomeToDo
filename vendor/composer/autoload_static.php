eae16cf492775496e397d791bdf0cf73e40bafe8
Sen Ueno <ueno@tricorder.jp>
2025-01-25 15:36:30 +0900

    PHPstan

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -8,6 +8,7 @@ class ComposerStaticInitfbee484fbcb98ba45566f17e8eb69e9b
 {
     public static $files = array (
         '6124b4c8570aa390c21fafd04a26c69f' => __DIR__ . '/..' . '/myclabs/deep-copy/src/DeepCopy/deep_copy.php',
+        '9b38cf48e83f5d8f60375221cd213eee' => __DIR__ . '/..' . '/phpstan/phpstan/bootstrap.php',
         'ec07570ca5a812141189b1fa81503674' => __DIR__ . '/..' . '/phpunit/phpunit/src/Framework/Assert/Functions.php',
     );
 
